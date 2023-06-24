ddf603354d5c8f5647f45574e6e5de3792181b7f
Javier Pumaccajia <jpumaccajia@vscperu.com>
2023-06-24 17:00:05 -0500

    Condiciones en Go

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github/MrJharts/godesde0/variables"
+	"runtime"
+	// "github/MrJharts/godesde0/variables"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(654)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// estado, texto := variables.ConviertoaTexto(654)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+	
+	if os := runtime.GOOS; os=="linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
